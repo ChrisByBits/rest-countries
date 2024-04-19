c23a3ff823e8d260e8251e3a5541066f54e4ec56
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2024-04-19 17:13:29 -0500

    first commit

---
+++ src/public/components/footer.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'footer-content'
+}
+</script>
+
+<template>
+  <footer class='bg-primary flex flex-column align-items-center justify-content-center'>
+      <p>{{$t('copyright', {brand: '2023 RESTCountries, inc'})}}</p>
+      <p>{{$t('develop', {developer: 'Christopher Lecca U202211399'})}}</p>
+  </footer>
+</template>
